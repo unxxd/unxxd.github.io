89ad2fa9802bbeea9058ce3c74525b14a0b5a404
DGFamily <dgfamily2124@gmail.com>
2022-04-18 03:11:29 +0400

    add

--- scripts/index.js
+++ scripts/index.js
@@ -136,5 +136,5 @@ window.ethereum.on('accountsChanged',  (accounts) => {
 
 // bind meta open button for mobile
 openMetaButton.addEventListener('click', () => {
-   window.location = "instagram://";
+   window.location = "https://google.com";
 })
