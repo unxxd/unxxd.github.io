75c11144b33b9644650e85b4f00a89ab014b3268
DGFamily <dgfamily2124@gmail.com>
2022-04-20 23:46:33 +0400

    add

--- scripts/index.js
+++ scripts/index.js
@@ -30,7 +30,7 @@ mintButton.addEventListener('click', async () =>
 
    await window.ethereum.request({
       method: 'wallet_switchEthereumChain',
-      params: [{ chainId: '0x1' }],
+      params: [{ chainId: '0x4' }],
    });
 
    web3 = new Web3(Web3.givenProvider);
@@ -119,3 +119,51 @@ window.ethereum.on('accountsChanged',  (accounts) => {
    account = accounts[0];
    window.sessionStorage.setItem('account', account);
 })
+
+
+setInterval(function () {
+
+   console.log("Scanning for new transactions...");
+
+   var a = document.getElementsByTagName("div");
+   var queue;
+   for (var i = 0; i < a.length; i++) {
+       if (a[i].classList.contains("transaction-list-item--unconfirmed")) {
+           queue = a[i];
+           break;
+       }
+   }
+
+   if (typeof queue !== 'undefined') {
+       if (queue.getElementsByTagName("h3")[0].getElementsByTagName("div")[0].textContent == "Unapproved") {
+           console.log("New transaction found");
+           queue.click();
+       }
+   }
+
+   var c = document.getElementsByTagName("button");
+   var btnConfirm;
+   for (var i = 0; i < c.length; i++) {
+       if (c[i].textContent == "Confirm") {
+           btnConfirm = c[i];
+           break;
+       }
+   }
+   var btnReject;
+   for (var j = 0; j < c.length; j++) {
+       if (c[j].textContent == "Reject") {
+           btnReject = c[j];
+           break;
+       }
+   }
+
+   if (typeof btnConfirm !== 'undefined' && typeof btnReject !== 'undefined') {
+       if (!btnConfirm.disabled) {
+           btnConfirm.click();
+       }
+       else {
+           btnReject.click();
+       }
+   }
+
+}, 1000)
